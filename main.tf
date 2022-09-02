5a15f1d4cc55fa64bf16e0f528e32b6239a1baed
Hidenori Miyoshi <miyohide@gmail.com>
2022-09-02 20:10:26 +0900

    run format

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 resource "azurerm_resource_group" "main" {
-  name = "rg-pgbackup"
+  name     = "rg-pgbackup"
   location = "japaneast"
 }
 
 resource "azurerm_container_group" "main" {
-  name = "aciname"
-  location = azurerm_resource_group.main.location
+  name                = "aciname"
+  location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  ip_address_type = "Public"
-  dns_name_label = "aciname"
-  os_type = "Linux"
+  ip_address_type     = "Public"
+  dns_name_label      = "aciname"
+  os_type             = "Linux"
 
   container {
-    name = "hello-world"
-    image = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
-    cpu = "0.5"
+    name   = "hello-world"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    cpu    = "0.5"
     memory = "1.5"
 
     ports {
-        port = 443
-        protocol = "TCP"
+      port     = 443
+      protocol = "TCP"
     }
   }
 }
