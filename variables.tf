722d43719e60a6278153fc936a5feb48ac27239b
Hidenori Miyoshi <miyohide@gmail.com>
2022-09-02 20:20:08 +0900

    create variable file

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  type        = string
+  default     = "miyohide"
+  description = "全てのリソースにつける接頭辞"
+}
+
+variable "location" {
+  type        = string
+  default     = "japaneast"
+  description = "リソースのリージョン"
+}
