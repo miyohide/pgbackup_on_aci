2d7efa3353565401b866d1832cb8fd88e2f05816
Hidenori Miyoshi <miyohide@gmail.com>
2022-09-04 09:31:36 +0900

    create aci with file share

--- main.tf
+++ main.tf
@@ -4,10 +4,24 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "random_string" "name" {
-  length = 4
-  upper = false
-  lower = true
+  length  = 4
+  upper   = false
+  lower   = true
   special = false
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "st${var.prefix}${random_string.name.result}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_share" "main" {
+  name                 = "aci-share"
+  storage_account_name = azurerm_storage_account.main.name
+  quota                = 5
 }
 
 resource "azurerm_container_group" "main" {
@@ -20,13 +34,23 @@ resource "azurerm_container_group" "main" {
 
   container {
     name   = "hello-world"
-    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    image  = "seanmckenna/aci-hellofiles"
     cpu    = "0.5"
-    memory = "1.5"
+    memory = "1.0"
 
     ports {
-      port     = 443
+      port     = 80
       protocol = "TCP"
+    }
+
+    volume {
+      name       = "logs"
+      mount_path = "/aci/logs"
+      read_only  = false
+      share_name = azurerm_storage_share.main.name
+
+      storage_account_name = azurerm_storage_account.main.name
+      storage_account_key  = azurerm_storage_account.main.primary_access_key
     }
   }
 }
