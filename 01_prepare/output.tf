15169da2663e5f5d84b747b80e9b35334c937b3c
Hidenori Miyoshi <miyohide@gmail.com>
2022-09-08 20:32:30 +0900

    create output

---
+++ 01_prepare/output.tf
@@ -0,0 +1,7 @@
+output "postgres_server_name" {
+  value = azurerm_postgresql_flexible_server.main.name
+}
+
+output "postgres_password" {
+    value = azurerm_postgresql_flexible_server.main.administrator_password
+}
