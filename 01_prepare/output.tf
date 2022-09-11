a2e1fd6e6863c5c86495db32dd82a866fbfecf5f
Hidenori Miyoshi <miyohide@gmail.com>
2022-09-11 14:18:30 +0900

    add output fields

--- 01_prepare/output.tf
+++ 01_prepare/output.tf
@@ -1,3 +1,7 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
 output "postgres_server_name" {
   value = azurerm_postgresql_flexible_server.main.name
 }
@@ -5,4 +9,8 @@ output "postgres_server_name" {
 output "postgres_password" {
   value     = azurerm_postgresql_flexible_server.main.administrator_password
   sensitive = true
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.main.name
 }
